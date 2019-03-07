fbc40a93074983f7a8aa73a70fc13d4293ffff4b
tlilecode <tlilecode@gmail.com>
2019-03-06 18:49:44 -0800

    Add file to initialize middleware and routes during application startup.

---
+++ api/startup/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const users = require("../routes/users");
+const workouts = require("../routes/workouts");
+const auth = require("../routes/auth");
+const error = require("../middleware/error");
+
+module.exports = function(app) {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use("/api/users", users);
+  app.use("/api/workouts", workouts);
+  app.use("/api/auth", auth);
+  app.use(error);
+};
