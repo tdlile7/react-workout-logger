5ed9ffe77ae1f71799c02d2f1d43b9f30896a72f
tlilecode <tlilecode@gmail.com>
2019-03-14 14:56:42 -0700

    Update viewWorkouts component to show each of the workouts, saved within the databases as individual modals, which, when selected, will pull up an edit workout template which will allow users to update previously saved workouts.

--- src/component/WorkoutApp/viewWorkouts.jsx
+++ src/component/WorkoutApp/viewWorkouts.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import Modal from "../common/modal";
 
 class ViewWorkouts extends Component {
   render() {
     const { workouts } = this.props;
+    const haveSavedWorkouts = workouts.length > 0;
+    const btnClasses = "btn btn-dark";
+
     return (
-      <div id="templates-container">
-        {workouts.length === 0 && <h1>No workouts have been saved</h1>}
-        {workouts.map(workout => {
+      <div id="workouts-container">
+        {!haveSavedWorkouts && <h1>No workouts have been saved</h1>}
+        {haveSavedWorkouts && <h1>Saved Workouts:</h1>}
+        {workouts.map((workout, index) => {
+          const target = workout.title.replace(" ", "");
           return (
-            <div className="row">
-              <h2>{workout.title}</h2>
-            </div>
+            <Modal
+              title={target}
+              body={<h3>Hello</h3>}
+              btnClasses={btnClasses}
+              btnCover={workout.title}
+            />
+            // <div className="row">
+            //   <h2>{workout.title}</h2>
+            // </div>
           );
         })}
       </div>
