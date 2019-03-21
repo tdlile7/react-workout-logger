34c787e42bd001201210b15e124a3982fe6a8887
tlilecode <tlilecode@gmail.com>
2019-03-21 14:02:44 -0700

    Update user mongoose model, where workouts are now embedded objects so saved workouts are associated with the respective logged in users.

--- api/models/user.js
+++ api/models/user.js
@@ -1,68 +1,70 @@
-const config = require("config");
-const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const exerciseSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
+    minlength: 5,
     maxlength: 50
   },
-  email: {
-    type: String,
+  reps: {
+    type: Number,
     required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true
+    min: 0,
+    max: 30
   },
-  password: {
-    type: String,
+  sets: {
+    type: Number,
     required: true,
-    minlength: 5,
-    maxlength: 1024
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false
+    min: 0,
+    max: 10
   }
 });
 
-userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign(
-    {
-      _id: this._id,
-      name: this.name,
-      email: this.email,
-      isAdmin: this.isAdmin
-    },
-    config.get("jwtPrivateKey")
-  );
-  return token;
-};
+const workoutSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 50
+  },
+  exercises: {
+    type: [exerciseSchema],
+    minlength: 1,
+    required: true
+  }
+});
 
-const User = mongoose.model("User", userSchema);
-
-function validateUser(user) {
+function validateWorkout(workout) {
   const schema = {
-    name: Joi.string()
-      .min(2)
+    title: Joi.string()
+      .min(1)
       .max(50)
       .required(),
-    email: Joi.string()
-      .min(5)
-      .max(255)
-      .required()
-      .email(),
-    password: Joi.string()
-      .min(5)
-      .max(255)
-      .required()
+    exercises: Joi.array()
+      .items(
+        Joi.object().keys({
+          name: Joi.string()
+            .required()
+            .min(5)
+            .max(50),
+          reps: Joi.number()
+            .min(0)
+            .max(30)
+            .required(),
+          sets: Joi.number()
+            .min(0)
+            .max(10)
+            .required()
+        })
+      )
+      .min(1)
   };
 
-  return Joi.validate(user, schema);
+  return Joi.validate(workout, schema);
 }
 
-exports.User = User;
-exports.validate = validateUser;
+exports.workoutSchema = workoutSchema;
+exports.validate = validateWorkout;
