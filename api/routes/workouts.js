91d712956353019d26b760cfb11bad475d460b91
tlilecode <tlilecode@gmail.com>
2019-03-21 14:04:31 -0700

    Update workout routes file to reflect that workouts are now embedded objects within the User model.

--- api/routes/workouts.js
+++ api/routes/workouts.js
@@ -1,64 +1,52 @@
-const { Workout, validate } = require("../models/workout");
+const { User } = require("../models/user");
+const { validate } = require("../models/workout");
 const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
-const validateObjectId = require("../middleware/validateObjectId");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const workouts = await Workout.find().sort("name");
-  res.send(workouts);
+//Send back all saved workouts on currently logged in user's account
+router.get("/", auth, async (req, res) => {
+  const userId = req.user._id;
+  const user = await User.findOne({ _id: userId });
+  res.send(user.workouts);
 });
 
+//Save new workout on logged in user's account
 router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
+  const newWorkout = req.body;
+  console.log(newWorkout);
+  const { error } = validate(newWorkout);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const workout = new Workout({
-    title: req.body.title,
-    exercises: req.body.exercises
+  const userId = req.user._id;
+  const user = await User.findOne({ _id: userId });
+  console.log(user);
+  user.workouts.push(newWorkout);
+  await user.save();
+
+  res.send(newWorkout);
+});
+
+//Delete a previously saved workout
+router.delete("/:id", [auth], async (req, res) => {
+  const userId = req.user._id;
+  const user = await User.findOne({ _id: userId });
+  const id = req.params.id;
+  let workoutToDelete = -1;
+  user.workouts.forEach((workout, index) => {
+    if (workout._id.toString() === id) {
+      workoutToDelete = index;
+    }
   });
-  await workout.save();
 
-  res.send(workout);
-});
-
-router.put("/:id", [auth], async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const workout = await Workout.findByIdAndUpdate(
-    req.params.id,
-    {
-      title: req.body.title,
-      exercises: req.body.exercises
-    },
-    { new: true }
-  );
-
-  if (!workout)
+  if (workoutToDelete < 0)
     return res.status(404).send("The workout with the given ID was not found.");
 
-  res.send(workout);
-});
+  const deletedWorkout = user.workouts.splice(workoutToDelete, 1);
+  await user.save();
 
-router.delete("/:id", [auth, admin], async (req, res) => {
-  const workout = await Workout.findByIdAndRemove(req.params.id);
-
-  if (!workout)
-    return res.status(404).send("The workout with the given ID was not found.");
-
-  res.send(workout);
-});
-
-router.get("/:id", validateObjectId, async (req, res) => {
-  const workout = await Workout.findById(req.params.id);
-
-  if (!workout)
-    return res.status(404).send("The workout with the given ID was not found.");
-
-  res.send(workout);
+  res.send(deletedWorkout);
 });
 
 module.exports = router;
